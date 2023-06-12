e3ded298842fbb2a38b23922e99e6f8afc366550
HeartBlack <gartoularachit@gmail.com>
2023-06-12 09:56:05 +0545

    fix: node modules home

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
       <div class="mb-2 sm:mb-0">
-        <a href="/home" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark">Home</a>
+        <Link to="/" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark">Home</Link>
       </div>
       <div>
-      <a href="/two" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Dashboard</a>
-        <a href="/one" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Login</a>
-        <a href="/three" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Sign Up</a>
-        <a href="/three" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Logout</a>
+      <Link to="/dashboard" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Dashboard</Link>
+        <Link to="/login" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Login</Link>
+        <Link to="/signup" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Sign Up</Link>
+        <Link to="/logout" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Logout</Link>
       </div>
     </nav>
   )
